c1b7fb3104e7bd91e2fe9ed69a15d8d3e749bb2c
DariaFerbey <dariaferbey@gmail.com>
2020-10-23 13:52:35 +0300

    Modified Task and State

--- src/main/java/com/softserve/itacademy/model/State.java
+++ src/main/java/com/softserve/itacademy/model/State.java
@@ -1,5 +1,53 @@
 package com.softserve.itacademy.model;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "states")
 public class State {
- //TODO
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private String name;
+
+    @OneToMany(mappedBy = "task")
+    private List<Task> tasks;
+
+    public State() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    @Override
+    public String toString() {
+        return "State{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", tasks=" + tasks +
+                '}';
+    }
 }
--- src/main/java/com/softserve/itacademy/model/Task.java
+++ src/main/java/com/softserve/itacademy/model/Task.java
@@ -1,5 +1,59 @@
 package com.softserve.itacademy.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="tasks")
 public class Task {
-//
+
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private Priority priority;
+    private long state_id;
+    private long todo_id;
+
+    public Task() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+
+    public long getState_id() {
+        return state_id;
+    }
+
+    public void setState_id(long state_id) {
+        this.state_id = state_id;
+    }
+
+    public long getTodo_id() {
+        return todo_id;
+    }
+
+    public void setTodo_id(long todo_id) {
+        this.todo_id = todo_id;
+    }
 }
